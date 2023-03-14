2d4662965e7b72880d4decac1a9505591dae7dcb
GabrielCNursa <gabriel@nursa.com>
2023-03-13 22:25:21 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+  <div>
+    <h1>Ari você é minha melhor parceria s2</h1>
+    <img src="https://media.tenor.com/dteyPLcdJJkAAAAC/cats-love.gif" alt="descrição do gif" width="400" height="300"/>
+  </div>
+  )
+}
+
+export default Home;

7b2733aa079790cbfddc7a52a845a41527366571
Gabriel Costa <49294653+Garccosta@users.noreply.github.com>
2023-07-18 23:03:26 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 function Home() {
   return (
-  <div>
-    <h1>Ari você é minha melhor parceria s2</h1>
-    <img src="https://media.tenor.com/dteyPLcdJJkAAAAC/cats-love.gif" alt="descrição do gif" width="400" height="300"/>
-  </div>
-  )
+    <div>
+      <h1>Ari você é minha melhor parceria s2</h1>
+      <img
+        src="https://media.tenor.com/dteyPLcdJJkAAAAC/cats-love.gif"
+        alt="descrição do gif"
+        width="400"
+        height="300"
+      />
+    </div>
+  );
 }
 
 export default Home;

63ac5a5de0b3b7694911a1cf13780992355c8370
Mac <mac@Macs-MacBook-Air.local>
2024-11-27 15:34:33 +0100

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,25 @@
-function status(req, res) {
-    return res.status(200).json({"msg": "Arri égua!"})
+import database from "infra/database";
+
+async function status(req, res) {
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  return res.status(200).json({
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(dbMaxConnections.rows[0].max_connections),
+        opened_connections: dbOpenedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
